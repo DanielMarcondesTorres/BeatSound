637b18d2f927fb19e80cd7e29c9e54fe81ae1c7f
irmaodopao <42471807+irmaodopao@users.noreply.github.com>
2020-03-11 08:27:55 -0300

    Add files via upload

---
+++ BeatSound-master/js/menu.js
@@ -0,0 +1,17 @@
+document.getElementById('menuToggle').addEventListener("click", function(e){
+  if (document.getElementById('menu').classList.contains("fechado")) {
+    document.getElementById('menu').classList.remove("fechado");
+    document.getElementById('menu').classList.add("aberto");
+  }else {
+    document.getElementById('menu').classList.add("fechado");
+    document.getElementById('menu').classList.remove("aberto");
+  }
+});
+
+
+document.getElementById('listaulas').addEventListener("click",function(){
+
+  var porcento = document.getElementById('percentual');
+
+
+});
