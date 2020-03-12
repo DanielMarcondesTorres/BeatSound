f0b0785e23731a3c186d2416f9325fbbfeef35f5
Daniel Torres <danzinhomt@gmail.com>
2020-03-11 22:36:06 -0300

    touch

--- js/menu.js
+++ js/menu.js
@@ -11,3 +11,57 @@ try {
 } catch (e) {
 
 }
+
+var offset = 0;
+var div = document.getElementById("menu");
+var isDown = false;
+// Add Pointer Event Listener
+
+document.getElementById('puxador').addEventListener('touchstart', function(e) {
+  isDown=true;
+  offset =event.touches[0].clientX;
+}, true);
+document.getElementById('puxador').addEventListener('touchend', function() {
+  isDown = false;
+}, true);
+
+document.addEventListener('mousemove', function(event) {
+  event.preventDefault();
+  if (isDown) {
+    if (event.clientX - offset > 100) {
+      document.getElementById('menu').classList.remove("fechado");
+      document.getElementById('menu').classList.add("aberto");
+      document.getElementById("inpu").checked = true;
+      isDown = false;
+      document.getElementById('puxador').style.left = "300px";
+    }else {
+      document.getElementById('menu').classList.add("fechado");
+      document.getElementById('menu').classList.remove("aberto");
+      document.getElementById("inpu").checked = false;
+      document.getElementById('puxador').style.left = "0px";
+    }
+    if (event.clientX - offset > 300 || event.clientX - offset <= 0 ) {
+      isDown = false;
+    }
+  }
+}, true);
+document.addEventListener('touchmove', function(event) {
+  console.log(isDown);
+  if (isDown) {
+    if (event.touches[0].clientX - offset > 100) {
+      document.getElementById('menu').classList.remove("fechado");
+      document.getElementById('menu').classList.add("aberto");
+      document.getElementById("inpu").checked = true;
+      isDown = false;
+      document.getElementById('puxador').style.left = "300px";
+    }else {
+      document.getElementById('menu').classList.add("fechado");
+      document.getElementById('menu').classList.remove("aberto");
+      document.getElementById("inpu").checked = false;
+      document.getElementById('puxador').style.left = "0px";
+    }
+    if (event.touches[0].clientX - offset > 300 || event.touches[0].clientX - offset <= 0 ) {
+      isDown = false;
+    }
+  }
+}, true);
