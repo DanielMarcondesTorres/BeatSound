fc16a20aed70fd9d0fc112fd3607f49a2cd8f329
irmaodopao <42471807+irmaodopao@users.noreply.github.com>
2020-03-12 21:26:25 -0300

    Add files via upload

---
+++ BeatSound-master/js/menu.js
@@ -0,0 +1,80 @@
+try {
+  document.getElementById('menuToggle').addEventListener("click", function(e){
+    if (document.getElementById('menu').classList.contains("fechado")) {
+      document.getElementById('menu').classList.remove("fechado");
+      document.getElementById('menu').classList.add("aberto");
+      document.getElementById('puxador').style.left = "300px";
+      document.getElementById('main').classList.add("mainaberta");
+    }else {
+      document.getElementById('menu').classList.add("fechado");
+      document.getElementById('menu').classList.remove("aberto");
+      document.getElementById('puxador').style.left = "0px";
+      document.getElementById('main').classList.remove("mainaberta");
+    }
+  });
+} catch (e) {
+
+}
+
+var offset = 0;
+var div = document.getElementById("menu");
+var isDown = false;
+document.getElementById('puxador').addEventListener('touchstart', function(e) {
+  isDown=true;
+  offset =event.touches[0].clientX;
+}, true);
+document.getElementById('puxador').addEventListener('touchend', function() {
+  isDown = false;
+}, true);
+
+document.getElementById('puxador').addEventListener('mousedown', function(e) {
+  isDown=true;
+  offset =event.clientX;
+}, true);
+document.getElementById('puxador').addEventListener('mouseup', function() {
+  isDown = false;
+}, true);
+
+document.addEventListener('mousemove', function(event) {
+  event.preventDefault();
+  if (isDown) {
+    if (event.clientX - offset > 100) {
+      document.getElementById('menu').classList.remove("fechado");
+      document.getElementById('menu').classList.add("aberto");
+      document.getElementById("inpu").checked = true;
+      isDown = false;
+      document.getElementById('puxador').style.left = "300px";
+      document.getElementById('main').classList.add("mainaberta");
+    }else {
+      document.getElementById('menu').classList.add("fechado");
+      document.getElementById('menu').classList.remove("aberto");
+      document.getElementById("inpu").checked = false;
+      document.getElementById('puxador').style.left = "0px";
+      document.getElementById('main').classList.remove("mainaberta");
+    }
+    if (event.clientX - offset > 300 || event.clientX - offset <= 0 ) {
+      isDown = false;
+    }
+  }
+}, true);
+document.addEventListener('touchmove', function(event) {
+  if (isDown) {
+    if (event.touches[0].clientX - offset > 100) {
+      document.getElementById('menu').classList.remove("fechado");
+      document.getElementById('menu').classList.add("aberto");
+      document.getElementById("inpu").checked = true;
+      isDown = false;
+      document.getElementById('puxador').style.left = "300px";
+      document.getElementById('main').classList.add("mainaberta");
+    }else {
+      document.getElementById('menu').classList.add("fechado");
+      document.getElementById('menu').classList.remove("aberto");
+      document.getElementById("inpu").checked = false;
+      document.getElementById('puxador').style.left = "0px";
+      document.getElementById('main').classList.remove("mainaberta");
+    }
+    if (event.touches[0].clientX - offset > 300 || event.touches[0].clientX - offset <= 0 ) {
+      isDown = false;
+    }
+  }
+}, true);
